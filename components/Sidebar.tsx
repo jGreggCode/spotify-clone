a9da7bc0e008626d21648aa33219f53178f3ea7c
jGreggCode <felicisimojv@gmail.com>
2024-12-23 13:48:09 +0800

    1.3 Initial commit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,43 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { BiBox, BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
+import Box from "./Box";
+
 interface SidebarProps {
   children: React.ReactNode;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
-  return <div>{children}</div>;
+  const pathName = usePathname();
+
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathName !== "/search",
+        href: "/",
+      },
+      {
+        icon: BiSearch,
+        label: "Search",
+        active: pathName === "/search",
+        href: "/search",
+      },
+    ],
+    [pathName]
+  );
+
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-2 bg-black h-full w-[300px] p-2">
+        <Box>Sidebar Navigation</Box>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
